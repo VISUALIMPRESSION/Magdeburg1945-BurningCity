b36fc721ec1f7613ab1339b795cf18dd21844768
PhilippZittlau <philipp.zittlau@t-online.de>
2023-10-20 09:30:01 +0200

    add final shipping

---
+++ Install_Multimersive-Android-Shipping-arm64.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1f15462bf40df593c16d5436b5a0163ec29fb5f5d3a772f8a14dfe3cfc31162
+size 2096
---
+++ Uninstall_Multimersive-Android-Shipping-arm64.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:049d4d12d3756f4b9a97cec16ecc5a3f37e733a7097474345ef451e36d544c73
+size 998
